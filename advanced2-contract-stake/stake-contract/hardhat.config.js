48d82b8417a31c511c687b546fd9d96b925a6b3f
leon.wei <383604438@qq.com>
2026-01-23 02:26:15 +0800

    commit

---
+++ advanced2-contract-stake/stake-contract/hardhat.config.js
@@ -0,0 +1,28 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+require("@openzeppelin/hardhat-upgrades");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: {
+    version: "0.8.22",
+    settings: {
+      optimizer: {
+        enabled: false,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    sepolia: {
+      //url: "https://sepolia.infura.io/v3/d8ed0bd1de8242d998a1405b6932ab33",
+      url: "https://sepolia.infura.io/v3/64820e5ca95c49c9b5022409694643ce",
+      // url: "https://eth-sepolia.g.alchemy.com/v2/" + process.env.ALCHEMY_API_KEY,
+      accounts: [process.env.PRIVATE_KEY],
+      gasPrice: 30000000000, // 30 Gwei
+    },
+  },
+  etherscan: {
+    // apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+};
